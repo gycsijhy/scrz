042d6040ccf2d095c3d5c3d242f5967c907b43a8
Jianghouyu <732057025@qq.com>
2016-08-29 17:09:43 +0800

    20160829

---
+++ src/main/java/com/gyhrss/ScrzApplication.java
@@ -0,0 +1,13 @@
+package com.gyhrss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrzApplication.class, args);
+
+	}
+}
